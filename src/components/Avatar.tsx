cc36d28da81e2f97d241edf0247f266cc53f3f76
spblue4422 <spblue4422@naver.com>
2021-10-20 18:49:04 +0900

    create Card

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import oc from 'open-color';
 
 export type Figure = 'circle' | 'square';
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
 import oc from 'open-color';
+
+type CardVariant =
+    | 'white'
+    | 'ocean'
+    | 'gray'
+    | 'yellow'
+    | 'pink'
+    | 'blue'
+    | 'purple'
+    | 'green'
+    | 'black';
+
+interface VariantStyle {
+    backgroundColor: React.CSSProperties['backgroundColor'];
+    color: React.CSSProperties['color'];
+}
+
+const CardVariantMap: Record<CardVariant, VariantStyle> = {
+    white: {
+        backgroundColor: oc.gray[0],
+        color: oc.gray[9]
+    },
+    ocean: {},
+    gray: {},
+    yellow: {},
+    pink: {},
+    blue: {},
+    purple: {},
+    green: {},
+    black: {}
+};
+
+export interface CardProps {
+    width: React.CSSProperties['width'];
+    height: React.CSSProperties['height'];
+    padding: React.CSSProperties['padding'];
+    fontWeight: React.CSSProperties['fontWeight'];
+}
+
+export interface CardWrapperProps {
+    theme: VariantStyle;
+}
+
+const CardWrapper = styled.div<CardWrapperProps>``;
+
+export const Card = React.forwardRef<HTMLDivElement, CardProps>(({ children, ...props }, ref) => {
+    return <CardWrapper ref={ref}>{children}</CardWrapper>;
+});
