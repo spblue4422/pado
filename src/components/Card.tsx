2d19600085a4d631a08cb77abe7f0ccf6006caad
spblue4422 <spblue4422@naver.com>
2021-10-23 19:11:49 +0900

    card 수정 및 checkbox 추가

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import oc from 'open-color';
 
-type CardVariant =
+export type Border = 'round' | 'angeld';
+
+export type CardVariant =
     | 'white'
     | 'ocean'
     | 'gray'
@@ -23,29 +25,78 @@ const CardVariantMap: Record<CardVariant, VariantStyle> = {
         backgroundColor: oc.gray[0],
         color: oc.gray[9]
     },
-    ocean: {},
-    gray: {},
-    yellow: {},
-    pink: {},
-    blue: {},
-    purple: {},
-    green: {},
-    black: {}
+    ocean: {
+        backgroundColor: oc.cyan[2],
+        color: oc.gray[9]
+    },
+    gray: {
+        backgroundColor: oc.gray[4],
+        color: oc.gray[9]
+    },
+    yellow: {
+        backgroundColor: oc.yellow[2],
+        color: oc.gray[9]
+    },
+    pink: {
+        backgroundColor: oc.pink[2],
+        color: oc.gray[9]
+    },
+    blue: {
+        backgroundColor: oc.blue[2],
+        color: oc.gray[9]
+    },
+    purple: {
+        backgroundColor: oc.violet[3],
+        color: oc.gray[9]
+    },
+    green: {
+        backgroundColor: oc.green[3],
+        color: oc.gray[9]
+    },
+    black: {
+        backgroundColor: oc.gray[9],
+        color: oc.gray[0]
+    }
 };
 
-export interface CardProps {
+export interface CardProps extends React.ComponentPropsWithoutRef<'div'> {
     width: React.CSSProperties['width'];
     height: React.CSSProperties['height'];
     padding: React.CSSProperties['padding'];
     fontWeight: React.CSSProperties['fontWeight'];
+    borderRadius: Border;
+    variant: CardVariant;
 }
 
-export interface CardWrapperProps {
+interface CardWrapperProps extends Omit<CardProps, 'variant'> {
     theme: VariantStyle;
 }
 
-const CardWrapper = styled.div<CardWrapperProps>``;
+const CardWrapper = styled.div<CardWrapperProps>`
+    width: ${(props) => props.width};
+    heigth: ${(props) => props.height};
+    padding: ${(props) => props.padding};
+    font-weight: ${(props) => props.fontWeight};
+    border-radius: ${(props) => (props.borderRadius === 'round' ? '10px' : '0px')}
+    background-color: ${(props) => props.theme.backgroundColor};
+    color: ${(props) => props.theme.color};
+`;
 
-export const Card = React.forwardRef<HTMLDivElement, CardProps>(({ children, ...props }, ref) => {
-    return <CardWrapper ref={ref}>{children}</CardWrapper>;
-});
+export const Card = React.forwardRef<HTMLDivElement, CardProps>(
+    ({ width, height, padding, fontWeight, borderRadius, variant, children, ...props }, ref) => {
+        const theme = CardVariantMap[variant];
+        return (
+            <CardWrapper
+                width={width}
+                height={height}
+                padding={padding}
+                fontWeight={fontWeight}
+                borderRadius={borderRadius}
+                theme={theme}
+                ref={ref}
+            >
+                {children}
+            </CardWrapper>
+        );
+    }
+);
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,3 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import oc from 'open-color';
+
+export type CheckboxVariant = 'normal' | 'ocean' | 'blue' | 'purple' | 'red' | 'green' | 'yellow';
+
+interface VariantStyle {
+    boxColor: React.CSSProperties['backgroundColor'];
+    hoverColor: React.CSSProperties['backgroundColor'];
+    focusColor: React.CSSProperties['backgroundColor'];
+}
+
+const CheckboxVariantMap: Record<CheckboxVariant, VariantStyle> = {
+    normal: {
+        boxColor: oc.gray[0],
+        hoverColor: oc.gray[2],
+        focusColor: oc.gray[4]
+    },
+    ocean: {
+        boxColor: oc.cyan[2],
+        hoverColor: oc.cyan[3],
+        focusColor: oc.cyan[5]
+    },
+    blue: {
+        boxColor: oc.blue[2],
+        hoverColor: oc.blue[3],
+        focusColor: oc.blue[4]
+    },
+    purple: {
+        boxColor: oc.violet[2],
+        hoverColor: oc.violet[3],
+        focusColor: oc.violet[4]
+    },
+    red: {
+        boxColor: oc.red[2],
+        hoverColor: oc.red[3],
+        focusColor: oc.red[4]
+    },
+    green: {
+        boxColor: oc.green[2],
+        hoverColor: oc.green[3],
+        focusColor: oc.green[5]
+    },
+    yellow: {
+        boxColor: oc.yellow[2],
+        hoverColor: oc.yellow[3],
+        focusColor: oc.yellow[5]
+    }
+};
+
+export interface CheckboxProps extends React.ComponentPropsWithoutRef<'input'> {
+    isChecked: boolean;
+    isDisabled: boolean;
+    isDefaultChecked: boolean;
+    variant: CheckboxVariant;
+}
+
+interface CustomCheckboxProps
+    extends Omit<CheckboxProps, 'variant' | 'isDisabled' | 'isDefaultChecked'> {
+    theme: VariantStyle;
+}
+
+const CheckboxWrapper = styled.div`
+    color: ${(props) => props.color};
+`;
+
+//display: none 으로 바꿔도될지 생각해보장
+const DefaultCheckbox = styled.input.attrs({ type: 'checkbox' })`
+    border: none;
+    height: 1px;
+    width: 1px;
+    padding: 0;
+    position: absolute;
+    white-space: nowrap;
+    overflow: hidden;
+    visiblity: hidden;
+`;
+
+const CustomCheckbox = styled.div<CustomCheckboxProps>`
+    display: inline-block;
+    width: 1px;
+    height: 1px;
+    background-color: ${(props) => (props.isChecked ? props.theme.boxColor : oc.gray[0])};
+    border-radius: 2px;
+
+    ${DefaultCheckbox}:disabled+& {
+        filter: blur(0.7);
+        background-color: ${oc.gray[0]};
+    }
+
+    ${DefaultCheckbox}:focus+& {
+        background-color: ${(props) =>
+            props.isChecked ? props.theme.boxColor : props.theme.focusColor};
+    }
+
+    &:hover {
+        background-color: ${(props) =>
+            props.isChecked ? props.theme.boxColor : props.theme.hoverColor};
+    }
+`;
+
+//label 추가해줘야함.
+export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+    (
+        { isDisabled = false, isDefaultChecked = false, variant = 'ocean', children, ...props },
+        ref
+    ) => {
+        const theme = CheckboxVariantMap[variant];
+        const [isChecked, setIsChecked] = useState<boolean>(isDefaultChecked);
+        const handleChkEvent = () => {
+            setIsChecked(!isChecked);
+        };
+
+        return (
+            <CheckboxWrapper {...props}>
+                <DefaultCheckbox
+                    disabled={isDisabled}
+                    checked={isChecked}
+                    onClick={handleChkEvent}
+                ></DefaultCheckbox>
+                <CustomCheckbox isChecked={isChecked} theme={theme} ref={ref}></CustomCheckbox>
+                {children}
+            </CheckboxWrapper>
+        );
+    }
+);
